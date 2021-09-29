3d0ebcfdf32742cb3d863691b1c1a6beef3f98fc
rakshit <rakshitsharma268@gmail.com>
2021-09-29 23:02:42 +0530

    props done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import Header from "./Header";
 import Footer from './Footer';
-import Note from './Note'
+import Note from './Note';
+import notes from '../notes';
+
+
 export default function App() {
     return (
         <div>
             <Header/>
-            <Note title="this is the title of notes" content="this is the content"/> 
+            {notes.map((noteItem)=>
+          <Note
+          key={noteItem.key}
+          title={noteItem.title} 
+          content={noteItem.content}
+          />
+    )}
             <Footer/>
         </div>
     )
