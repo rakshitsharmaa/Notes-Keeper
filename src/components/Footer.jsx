354e3389e6d99bd33982d2ffe3583f38fcfb41e3
rakshit <rakshitsharma268@gmail.com>
2021-09-29 18:42:55 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Footer() {
+    const date=new Date().getFullYear();
+    return (
+        <footer>
+              <p>copyright {date} all rights reserved to rakshit sharma</p>
+              </footer>
+          
+      
+    )
+}
---
+++ src/components/Note.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Note() {
+    return (
+        <div className="note">
+            <h1>title</h1>
+            <p>hello this is the data of this note</p>
+        </div>
+    )
+}
