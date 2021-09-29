55eee4499461c7d00c029c3059d121e237ee13b7
rakshit <rakshitsharma268@gmail.com>
2021-09-30 01:14:26 +0530

    final tast is about to begin

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,7 @@
-import React from 'react'
 import Header from "./Header";
 import Footer from './Footer';
 import Note from './Note';
 import notes from '../notes';
-
-
 export default function App() {
     return (
         <div>
