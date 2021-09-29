a45cead1c4714c1105c3461f30019f4042bcb49b
rakshit <rakshitsharma268@gmail.com>
2021-09-30 03:35:51 +0530

    useState hook used

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,15 @@ import Header from "./Header";
 import Footer from './Footer';
 import Note from './Note';
 import notes from '../notes';
+import CreateArea from "./createArea";
 export default function App() {
+  function addNote(note){
+    console.log(note);
+    }
     return (
         <div>
             <Header/>
+            <CreateArea/>
             {notes.map((noteItem)=>
           <Note
           key={noteItem.key}
---
+++ src/components/createArea.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { useState } from "react";
+function CreateArea() {
+    const [note,setNote]= useState(
+        { title:"",
+        content:""}
+    )
+    function handleChange(event){
+        const {name,value}=event.target;
+        setNote(prevNote=>{
+            return{
+                ...prevNote,
+                [name]:value
+            }
+        })
+    }
+    function submitNote(event){
+         event.preventDefault();
+
+    }
+  return (
+    <div>
+      <form>
+        <input name="title" 
+        onChange={handleChange}
+         placeholder="Title"
+          value={note.title} />
+        <textarea name="content" 
+        onChange={handleChange}
+         placeholder="Take a note..." rows="3" 
+         value={note.content} />
+        <button onClick={submitNote}>Add</button>
+      </form>
+    </div>
+  );
+}
+export default CreateArea;
